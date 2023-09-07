f462d216abc40381b0c0e05da00609442ca0e3f1
auztinc1 <aaustinc@live.com>
2023-09-07 00:31:28 -0400

    Well it works

--- src/Game.jsx
+++ src/Game.jsx
@@ -20,7 +20,8 @@ export default function Game() {
   useEffect(() => {
     async function fetchCard() {
       setCardBank(Array.from(document.getElementsByClassName("img")))
-      fetch(`https://pixabay.com/api/?key=35904460-6da0f483724d8177c3f681e67&q=${input.val}&orientation=horizontal&per_page=100`)
+      let page = Math.ceil(Math.random()*2);
+      fetch(`https://pixabay.com/api/?key=35904460-6da0f483724d8177c3f681e67&q=${input.val}&orientation=horizontal&per_page=15&page=${page}`)
           .then((response) => response.json())
           .then((data) => {
             setPicBank(data.hits)
@@ -31,22 +32,34 @@ export default function Game() {
       }, [input])
 
     useEffect(() => {
-      if(picBank.length === 100 ){
-        if(cardBank.length < 30){
-          let curRan = picBank[Math.round(Math.random() * picBank.length -1 )]
-          setCardBank(cards=>[...cards, {img: curRan.largeImageURL, id: curRan.id}, {img: curRan.largeImageURL, id: curRan.id}])
+      if(picBank.length === 15 ){
+        let cb = [];
+          for (let i = 0; i < 15; i++) {
+            let curRan = picBank[Math.floor(Math.random() * (picBank.length - 1) )]
+            
+            if (curRan === undefined) {
+              console.log("BLANKS")
+          }
+
+          
+          if(curRan != undefined){
+            cb.push(...[{img: curRan.largeImageURL, id: curRan.id}, {img: curRan.largeImageURL, id: curRan.id}])
+          } else {
+            
+          }
         }
-      }
-      if(cardBank.length === 30 && count.current !== 3){
-          let tempArr = cardBank
-          .map(card=>({ card, sort: Math.random() }))
-          .sort((a, b) => a.sort - b.sort)
-          .map(({ card }) => card)
-          setCardBank(tempArr)
-        count.current++
+        
+        for (let i = 0; i < 3; i++) {
+            let tempArr = cb
+            .map(card=>({ card, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ card }) => card)
+            cb = tempArr;
+        }
+        setCardBank(cb);
       }
 
-  }, [picBank, cardBank])
+  }, [picBank])
 
 
   //    --- Compare cards currently in Active array
@@ -74,23 +87,29 @@ export default function Game() {
     event.preventDefault()
     if(inGame){
       if(window.confirm("This will reset your current game!")){
-        Array.from(document.getElementsByClassName("card-inner")).forEach((e)=>{
-          e.style.transform = "rotateY(180deg)"
-        })
-        setTimeout(()=>{
-        setCardBank([])
-          setInput({val: event.target.input.value})
-          // event.target.input.value = ""
-        }, 700)
+        reset(event)
       } else return
+    } else {
+      reset(event)
     }
+  }
+  function reset(ev){
+    // Array.from(document.getElementsByClassName("card-inner")).forEach((e)=>{
+    //   e.style.transform = "rotateY(180deg)"
+    // })
+    setTimeout(()=>{
+      setInput({val: ev.target.input.value})
+      // setCardBank([])
+      // event.target.input.value = ""
+    }, 700)
+    
   }
 
   return (<>
     <form id="form" onSubmit={handleSubmit}>
       <button>New Game</button>
       <label htmlFor="input">Choose Your Images!</label>
-      <input type="text" placeholder="Image Search" name="input" id="input"/>
+      <input type="text" placeholder="Puppies.." name="input" id="input"/>
     </form>
 
     <div id='game'>
